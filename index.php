194c8c01ad880c91371eea11ff18babc9a8a7f0a
Francisco <fran.sango.02@gmail.com>
2023-03-06 12:36:24 +0100

    Añado head

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ $resultado = $mysqli->query($sql);
 <!doctype html>
 <html lang="es">
 
-<head>
+<head> 
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
